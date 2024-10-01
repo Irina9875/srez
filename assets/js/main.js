9469d3add40922b5b2c2c8c53bd9253f3f4a3100
“Irina9875” <khozhimuratovairina@mail.ru>
2024-10-01 16:22:14 +0300

    iri_srez

---
+++ assets/js/main.js
@@ -0,0 +1,133 @@
+'use strict'
+
+// меню
+let menuElems = document.querySelectorAll('.menu__elem');
+
+menuElems.forEach(menuElem => {
+    let submenu = menuElem.querySelector('.submenu');
+    let btn = menuElem.querySelector('.menu__btn');
+
+    menuElem.addEventListener('mouseenter', function(){
+        submenu.classList.add('active')
+        btn.classList.add('active')
+    })
+
+    menuElem.addEventListener('mouseleave', function(){
+        submenu.classList.remove('active')
+        btn.classList.remove('active')
+    })
+})
+
+
+// слайдер 
+ 
+let currentIndex = 0; 
+const slides = document.querySelectorAll(".slide"); 
+const dots = document.querySelectorAll(".dot"); 
+ 
+function showSlide(index) { 
+    if (index >= slides.length) { 
+        currentIndex = 0; 
+    } else if (index < 0) { 
+        currentIndex = slides.length - 1; 
+    } else { 
+        currentIndex = index; 
+    } 
+ 
+    slides.forEach((slide, i) => { 
+        slide.style.display = "none"; 
+        dots[i].classList.remove("active"); 
+    }); 
+ 
+    slides[currentIndex].style.display = "block"; 
+    dots[currentIndex].classList.add("active"); 
+} 
+ 
+ 
+function moveSlide(step) { 
+    showSlide(currentIndex + step); 
+} 
+ 
+function currentSlide(index) { 
+    showSlide(index); 
+} 
+ 
+function initSlider() { 
+    showSlide(currentIndex); 
+} 
+ 
+dots.forEach((dot, index) => { 
+    dot.addEventListener("click", () => currentSlide(index)); 
+}); 
+ 
+document.addEventListener("DOMContentLoaded", initSlider);
+
+
+// модальное
+var modal = document.getElementById("modal");
+var span = document.getElementById("closeModal");
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+setTimeout(openModal, 3000);
+
+
+// Number
+let phoneBtn = document.querySelector(".phone");
+let sale = document.querySelector(".sale");
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        phoneBtn.classList.add("show");
+      } else {
+        phoneBtn.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observer.observe(sale);
+// вкладки
+let tabBtns = document.querySelectorAll('.tab__btn')
+let tabTexts = document.querySelectorAll('.tab__text')
+
+tabBtns.forEach((item, index) => {
+    item.addEventListener('click', function () {
+        document.querySelector('.tab__btn.active').classList.remove('active');
+        item.classList.add('active');
+
+        document.querySelector('.tab__text.active').classList.remove('active');
+        tabTexts[index].classList.add('active')
+    })
+})
+// вопросы аккардион
+document.querySelectorAll('.faq-card').forEach(card => {
+    card.addEventListener('click', function () {
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            document.querySelectorAll('.faq-card').forEach(card => {
+                card.classList.remove('active');
+            });
+            this.classList.add('active');
+        }
+    });
+});
